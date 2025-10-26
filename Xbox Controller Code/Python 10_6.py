fb6abf37ef58a35bef010f6f4410a20d6f6f8a69
Kwaku Kwabia <kwkwabia@gmail.com>
2025-10-26 11:39:56 -0400

    Update Python 10_6.py

--- Xbox Controller Code/Python 10_6.py
+++ Xbox Controller Code/Python 10_6.py
@@ -35,24 +35,24 @@ def motor_off():
   GPIO.output(IN2, GPIO.LOW)
   pwm.ChangeDutyCycle(0)
 
-# def cleanup_and_exit(code=0):
-  # try:
-   # motor_off()
-   # pwm.stop()
-   # GPIO.cleanup()
-  # finally:
-   # sys.exit(code)
+def cleanup_and_exit(code=0):
+  try:
+   motor_off()
+   pwm.stop()
+   GPIO.cleanup()
+  finally:
+   sys.exit(code)
 
 def main():
-  # signal.signal(signal.SIGINT, lambda s,f: cleanup_and_exit(0))
-  # signal.signal(signal.SIGTERM, lambda s,f: cleanup_and_exit(0))
+  signal.signal(signal.SIGINT, lambda s,f: cleanup_and_exit(0))
+  signal.signal(signal.SIGTERM, lambda s,f: cleanup_and_exit(0))
   
   dev = InputDevice('/dev/input/event10')
   print(f"Using: {dev.path} ({dev.name})")
   print("start")
   
   A_BTN = ecodes.BTN_SOUTH
-  PROBLEM_BTN = A_BTN
+  
 
   motor_off()
   for event in dev.read_loop():
@@ -66,19 +66,10 @@ def main():
         elif keyevent.keystate == 0:
           motor_off()
           print("off")
-      elif keyevent.scancode == PROBLEM_BTN:
-        pass
 
 if__name__ == "__main__":
       try:
         main()
-      except KeyboardInterrupt:
-        print("Keyboard Interrupt")
       except Exception as e:
         print("Error:", e)
-      finally:
-        print("GPIO Cleanup")
-        motor_off()
-        pwm.stop()
-        GPIO.cleanup()
-        print("done")
+        cleanup_and_exit(1)
